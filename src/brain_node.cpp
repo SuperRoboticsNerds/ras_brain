355dfc14b1a8908ebd7f53b8369c2f353e97a3f6
Erik Sillen <esillen@kth.se>
2015-12-01 18:27:42 +0100

    got somthing to work

--- src/brain_node.cpp
+++ src/brain_node.cpp
@@ -35,6 +35,8 @@
 #include "tf/message_filter.h"
 #include "message_filters/subscriber.h"
 
+#define ROT_DURATION  0.2
+#define FORWARD_DURATION  1.0
 
 
 class BrainNode
@@ -55,7 +57,8 @@ public:
     visualization_msgs::MarkerArray marker_object_vec;
 
 
-
+    bool got_path;
+    bool pos_received ;
 
     ros::NodeHandle n;
 
@@ -73,6 +76,8 @@ public:
     {
         n = ros::NodeHandle("~");
         object_seen = false;
+        got_path=false;
+        path_node_index =0;
         //The rows and cols needs to be one bigger than the length of the outer walls
         //grid_cost_map_pub = n.advertise<std::vector< std::vector<struct position_node> >("test",1);
         path_sub = n.subscribe<nav_msgs::GridCells>( "/nodes_generator/path", 1,&BrainNode::path_vector_function,this);
@@ -84,7 +89,7 @@ public:
         twist_pub = n.advertise<geometry_msgs::Twist>("/motor_controller/twist",100);
         marker_object_pub = n.advertise<visualization_msgs::MarkerArray>( "/object_to_rvis_grid_map", 1);
         object_pos_pub= n.advertise<geometry_msgs::PointStamped>( "/object_pos", 1);
-        
+        pos_received = false;
     }
     ~BrainNode()
     {
@@ -95,21 +100,43 @@ public:
 
 void path_vector_function(nav_msgs::GridCells msg){
         paths = msg.cells;
+        //path_node_index=0;
+        got_path=true;
    }
 
 void get_next_path(){
-    geometry_msgs::Point pt = paths[0];
+    std::cout << "-----------here 3 -----------"<< std::endl;
+    geometry_msgs::Point pt = paths[path_node_index];
     newx = pt.x;
     newy = pt.y;
-    paths.erase(paths.begin());
+    path_node_index++;
+    //paths.erase(paths.begin());
+    got_path=false;
+    std::cout << "-----------here 4 -----------"<< std::endl;
+}
+
+void check_at_correct_place(){
+    std::cout << "-----------correct 1 -----------"<< std::endl;
+    for(int i = -5; i == 5;i++){
+        std::cout << "-----------correct k:i -----------"<< std::endl;
+            if(((robot_x+i)==newx) && ((robot_y+i)==newy)){
+                std::cout << "-----------check ************************** -----------"<< std::endl;
+                get_next_path();
+
+            }
+    }
+
 }
 
 
 
+
 void move_function(){
+    //go_forward(1.0);
     //while not close
+    std::cout << "inside"<< std::endl;
     double alpha = atan2(newy-robot_y,newx-robot_x);
-    if(!(std::abs(alpha-robot_theta) < 0.2 || std::abs(2*M_PI-robot_theta+alpha) < 0.2)){
+    if((std::abs(alpha-robot_theta) > 0.2) || (std::abs(2*M_PI-robot_theta+alpha) > 0.2)){
         if(std::abs(alpha-robot_theta) < std::abs(2*M_PI-robot_theta+alpha)){
             rotate(alpha-robot_theta);
         }
@@ -117,13 +144,19 @@ void move_function(){
             rotate(2*M_PI-robot_theta+alpha);
         }
     }
+    std::cout << "-----------middle -----------"<< std::endl;
+    //ros::spinOnce();
     double dist_to_goal = std::sqrt((newx-robot_x)*(newx-robot_x) + (newy-robot_y)*(newy-robot_y));
-    if (!dist_to_goal<0.05){
+    if (dist_to_goal>20){
         go_forward(std::max(0.2,dist_to_goal));
     }
+    std::cout << "-----------middle 2-----------"<< std::endl;
+    /*
     if(object_seen){
         move_to_object();
-    }
+    }*/
+    check_at_correct_place();
+    std::cout << "-----------end -----------"<< std::endl;
 }
 
 void move_to_object(){
@@ -141,15 +174,34 @@ void move_to_object(){
 
 void rotate(double angle){
     geometry_msgs::Twist twist_msg;
-    twist_msg.angular.z = 0.2; // [m/s]
+    twist_msg.linear.x = 0.0;
+    if (angle<0.1){
+    twist_msg.angular.z = -3.0;}
+    else if (angle>0.1){
+        twist_msg.angular.z = 3.0;
+    }
+    else{
+        twist_msg.angular.z = 0.0;
+    }
+    double start_time =ros::Time::now().toSec();
+    while (ros::Time::now().toSec()-start_time<ROT_DURATION){
+    twist_pub.publish(twist_msg);
+}
 
+    twist_msg.angular.z = 0.0;
     twist_pub.publish(twist_msg);
 }
 
 void go_forward(double distance){
     geometry_msgs::Twist twist_msg;
-    twist_msg.linear.x = M_PI/4; // [rad/s]
+    twist_msg.linear.x = 0.4;
+    twist_msg.angular.z = 0.0;
+    double start_time =ros::Time::now().toSec();
+    while (ros::Time::now().toSec()-start_time<FORWARD_DURATION){
+    twist_pub.publish(twist_msg);
+}
 
+    twist_msg.linear.x = 0.0;
     twist_pub.publish(twist_msg);
 
 }
@@ -273,19 +325,30 @@ void object_detected_function(ras_msgs::Object_id msg){
 
     }
 void current_robot_position_function(localization::Position msg){
-    robot_x=msg.x;
-    robot_y=msg.y;
+    //std::cout << "pos recived"<< std::endl;
+    robot_x=msg.x*100.0;
+    robot_y=msg.y*100.0;
     robot_theta=msg.theta;
 
+    pos_received = true;
     }
 
-
+int pos_received_func(){
+    if (pos_received==true){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
 private:
 
     bool object_seen;
+
  int rows;
  int col; 
  int object_counter;
+ int path_node_index;
  
  double xEnd;
  double xStep;
@@ -306,7 +369,9 @@ double robot_theta;
 int main(int argc, char **argv)
 {
     int counter;
+    int got_path_game;
     counter=0;
+    got_path_game=0;
     ros::init(argc, argv, "brain_node");
     BrainNode brain_node;
    
@@ -318,14 +383,28 @@ int main(int argc, char **argv)
     
     while(brain_node.n.ok())
     {   
+        if(brain_node.got_path==true){
+           //brain_node.go_forward(10);
+            std::cout << "-----------here 1 -----------"<< std::endl;
+           brain_node.get_next_path();
+           std::cout << "------------Here 2-------------"<< std::endl;
+           got_path_game=1;
+        }
 
 
+  
+
+        if((brain_node.pos_received_func() == 1)&&(got_path_game==1)){
+            std::cout << "-----------here 5 -----------"<< std::endl;
+            brain_node.move_function();
+            std::cout << "-----------here 6 -----------"<< std::endl;
+        }
 
 
         counter++;
         ros::spinOnce();
         loop_rate.sleep();
-    }
+     }
 
     return 0;
 }
